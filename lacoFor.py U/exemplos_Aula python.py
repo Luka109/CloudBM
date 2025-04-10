0756ea482b4a62189b26b092d91823b9ad19519d
UniFanor <UniFanor@FNS-WS-036735.EDUC.ACAD>
2025-04-10 20:44:18 -0300

    aula06

---
+++ lacoFor.py U/exemplos_Aula python.py
@@ -0,0 +1,11 @@
+# Nesse programa, iremos demonstrar aplicação básica do laço ou loop for
+# laço for  
+frutas = ['maçã', 'banana', 'laranja', 'morango']
+for revelino in range(1000):
+    print(f"Ola, mundo {revelino}")
+    lista= ["Carlos", "Luka", "Paulo", "Erik", "Adler", "Revelino", "Jose",
+             "Davi", "Vitor"]
+ for nome in lista
+    print(nome)
+
+print(lista[0])
